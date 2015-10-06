992fe51b9d8120faf41214aae19d8022b22947bc
mazander <mazander.code@gmail.com>
2015-10-06 14:51:11 +0300

    LWJGL 2.9.3, Quava 18.0, junit 4.12, jdom2 2.0.6, jaxen 1.1.6

--- ardor3d-lwjgl/src/main/java/com/ardor3d/scene/state/lwjgl/util/LwjglTextureUtil.java
+++ ardor3d-lwjgl/src/main/java/com/ardor3d/scene/state/lwjgl/util/LwjglTextureUtil.java
@@ -3,7 +3,7 @@
  *
  * This file is part of Ardor3D.
  *
- * Ardor3D is free software: you can redistribute it and/or modify it 
+ * Ardor3D is free software: you can redistribute it and/or modify it
  * under the terms of its license which may be found in the accompanying
  * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
  */
@@ -381,7 +381,7 @@ public abstract class LwjglTextureUtil {
             case R32I:
             case R32UI:
             case CompressedRed:
-                return ARBTextureRg.GL_RED;
+                return GL11.GL_RED;
             case RG8:
             case RG16:
             case RG16F:
